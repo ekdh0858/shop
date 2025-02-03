3b1b15200e43739eb3d80e7bdf94d5a78a7e2cf0
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-03 15:24:00 +0900

    관심상품 조회 API

--- src/main/java/com/sparta/shop/controller/ProductController.java
+++ src/main/java/com/sparta/shop/controller/ProductController.java
@@ -3,8 +3,13 @@ package com.sparta.shop.controller;
 import com.sparta.shop.dto.ProductMypriceRequestDto;
 import com.sparta.shop.dto.ProductRequestDto;
 import com.sparta.shop.dto.ProductResponseDto;
+import com.sparta.shop.entity.Product;
+import com.sparta.shop.repository.ProductRepository;
 import com.sparta.shop.service.ProductService;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductRepository productRepository;
 
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
@@ -27,7 +33,12 @@ public class ProductController {
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
         return productService.update(id,requestDto);
+    }
 
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts(){
+
+        return productService.getProducts();
 
     }
 }
